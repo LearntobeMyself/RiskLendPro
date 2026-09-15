a6730a7207dd863ac90b42ef54435429e12b2e10
annnstar <2587167885@qq.com>
2026-09-15 18:47:12 +0800

    Create BCardFeatureSnapshotMapper.java

---
+++ services/common-runtime/src/main/java/org/example/risklendpro/risk/mapper/BCardFeatureSnapshotMapper.java
@@ -0,0 +1,9 @@
+package org.example.risklendpro.risk.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.example.risklendpro.risk.entity.BCardFeatureSnapshot;
+
+@Mapper
+public interface BCardFeatureSnapshotMapper extends BaseMapper<BCardFeatureSnapshot> {
+}
